82162f2f95178ffc86f90acc4adc5aa0b19cc9c9
Dmitry <dbk76@mail.ru>
2022-12-28 17:02:55 +0500

    experts search

--- pages/experts.js
+++ pages/experts.js
@@ -13,7 +13,13 @@ import { useTranslation } from 'react-i18next'
 import axios from 'axios';
 import { createStrapiAxios } from '../utils/strapi';
 
-import { Pagination } from 'antd';
+import { Pagination, Row, Col } from 'antd';
+
+import SearchField from '../src/components/searchField/searchField';
+/* import SideFilter from '../src/components/SideFilter/sideFilter'; */
+import PageContainer from '../src/components/layouts/pageContainer';
+import PageHeader from '../src/components/layouts/pageHeader';
+import { useThemeContext } from '../src/context/themeContext';
 
 const PAGESIZE = 3;
 
@@ -22,33 +28,34 @@ export default function Experts({serverExperts}) {
 
   const {currentUser} = useUserContext();
 
+  const THEME = useThemeContext();
+
   const router = useRouter();
   const [page, setPage] = useState(parseInt(router.query.page)?? 1);
 
-  /* const getExperts = async (cPage) =>
-    await createStrapiAxios()
-          .get(`/users?limit=${pageSize}&start=${(cPage-1)*pageSize}`)
-          .then(res => res.json());
-   */
- // const {data = {}} = useQuery(
-  const {isSuccess,
-         isLoading,  
-         data: experts = []  }  = useQuery(
-                                                        ["experts", page],
-                                                        async () =>
-                                                        await createStrapiAxios()
-                                                              .get(`/users?limit=${PAGESIZE}&start=${(page-1)*PAGESIZE}`)
-                                                              .then(res => res.data),
-                                                        {
-                                                          keepPreviousData: true,
-                                                          refetchOnMount: false,
-                                                          refetchOnWindowFocus: false,
-                                                          initialData: serverExperts 
-                                                        }
-                                                      );
+  const {search, branch, specialization, city} = router.query;
+   
+    const {isSuccess,
+           isLoading,  
+           data: experts = []  }  = useQuery(["experts", page, search, branch, specialization, city],
+                                                () => getUsers({page, search, branch, specialization, city}),
+                                              {
+                                                keepPreviousData: true,
+                                                refetchOnMount: false,
+                                                refetchOnWindowFocus: false,
+                                                initialData: serverExperts 
+                                              }
+                                            );
+
   function handlePaginationChange(value) {
+    
+    router.query.page = value;
+    const url = {
+      pathname: router.pathname,
+      query: router.query
+    }
+    router.push(url, undefined, { shallow: true });
     setPage(value);
-    router.push(`experts/?page=${value}`, undefined, { shallow: true });
   }
 
    useEffect(() => {
@@ -69,19 +76,31 @@ export default function Experts({serverExperts}) {
   
  }
 
+ const p = THEME?.gutters?.gorizontal[THEME?.id];
+
+ const pad = `${p}px`;
+
   return (
 
-    <div className={styles.container}>
+   
+    <>
       
-      <div className={styles.main}>
-        <h1 className={styles.title}>
-         <p> EXPERTS page</p>
-         <p>{currentUser?.name}</p>
-         <p>{currentUser?.surname}</p>
-         <p>{currentUser?.avatar?.url}</p>
-        </h1>
+        <PageHeader 
+          /* title={t('headers.experts')} */
+          title="Эксперты"  
+        />
+        <PageContainer>
+         
+                 <Col  xs={24} sm={24} md={16} lg={18} xl={18} >
+     
+                  <SearchField 
+                    style={{ width: "100%", marginTop: "0px", borderRadius: "10px", marginBottom: pad}} 
+                    //placeholder={t('placeholders.searchExperts')}
+                    placeholder="Введите полностью или частично имя или фамилию"
+                    />  
+                {/* <ExpertList /> */}
 
-        {isSuccess && <div>{EXPERTS}</div>}
+                {isSuccess && <div>{EXPERTS}</div>}
         {isLoading && <div style={{color: "red"}}>LOADING</div>}
 
         <Pagination 
@@ -91,25 +110,71 @@ export default function Experts({serverExperts}) {
           total={20}
           pageSize={PAGESIZE}
           />
+                </Col>
+                <Col xs={0} sm={0} md={8} lg={6} xl={6} >
+              {/*      <SideFilter show = {{
+                        city: true,
+                        budget: false,
+                        branch: true}
+                    }/>   */}
+                </Col>
   
-       </div>
-    </div>
+            {/* </Row> */}
+  
+       </PageContainer>
+    </>
 
+        
+  
+   
   )
 }
 
+async function getUsers({page, search, branch, specialization, city}) {
+
+  let searchArr = []; 
+
+  if (search) {
+    searchArr.push(`filters[$or][0][name][$containsi]=${search}&filters[$or][1][surname][$containsi]=${search}`);
+  }
+  if (branch) {
+    searchArr.push(`_where[branches.id]=${branch}`);
+  }
+  if (specialization) {
+    searchArr.push(`_where[specialization.id]=${specialization}`);
+  }
+  if (city) {
+    searchArr.push(`_where[city]=${city}`);
+  }
+
+  let countSearchStr = searchArr.join("&");
+
+  searchArr.push(`limit=${PAGESIZE}&start=${(page-1)*PAGESIZE}`);
+    
+  //searchArr.push(`sort[0]=rating:desc`);
+
+  let searchStr = searchArr.join("&");
+
+  const data = await createStrapiAxios()
+ .get(`/users?${searchStr}`)
+ .then(res => res.data)
+
+ return (data) 
+
+};
+
+
+
 export async function getServerSideProps(context) {
   let page = 1;
-  const pageSize = 3;
+ 
   if (context.query.page) {
     page = parseInt(context.query.page);
   }
+  const {search, branch, specialization, city} = context.query;
 
-  const res = await createStrapiAxios()
-  .get(`/users?limit=${PAGESIZE}&start=${(page-1)*PAGESIZE}`)
-
-  const data = await res.data;
+  const data = await getUsers({page, search, branch, specialization, city});
 
   
-  return { props: { serverExperts: data } }  // data
+  return { props: { serverExperts: data } }  
 }
---
+++ src/components/searchField/searchField.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect}from 'react';
+import { useRouter } from 'next/router';
+import { Input} from 'antd';
+import styles from "./searchField.module.less";
+import { SearchOutlined } from '@ant-design/icons';
+
+import {useThemeContext} from '../../context/themeContext'
+
+
+
+export default function SearchField ({placeholder = '', style, resetPage=true}) {
+
+    const router = useRouter();
+
+    const [value, setValue] = useState();
+    
+    const {size, elementHeight} = useThemeContext();
+
+    const search = router.search;
+   
+    const onInputChange = (e) => {
+        const nV = e.target.value;
+        setValue(nV);
+        if (nV ==='') {
+            delete router.query.search 
+            router.query.page = 1
+            const url = {
+                pathname: router.pathname,
+                query: router.query
+              }
+          router.push(url, undefined, { shallow: true })
+          }
+    };
+
+    const onSearch = () => {
+        if (value && value !== '') {
+            router.query.search = value
+           if (resetPage) {
+             router.query.page = 1;
+           }
+           const url = {
+            pathname: router.pathname,
+            query: router.query
+          }
+      router.push(url, undefined, { shallow: true })
+
+        }
+        
+      };
+
+      useEffect(() => {
+        if (router.query.search) {
+          setValue(router.query.search);
+        }
+      }, [router.query.search]); 
+
+
+    return (
+        < div className={styles.wrapper} style = {style}>
+            <Input size = {size} 
+                   style = {{width: '100%'}} 
+                   onChange={onInputChange} 
+                   onPressEnter={onSearch} 
+                   value = {value} 
+                   placeholder={placeholder} 
+                 //  defaultValue={defaultValue ?? search}
+                   bordered= {false}
+                />
+            <div 
+                className = {value?.length>0 ? styles.blueIcon : ''}
+                style={{width: elementHeight, 
+                         height: elementHeight, 
+                         display: 'flex', 
+                         justifyContent: 'center', 
+                         alignItems: 'center',
+                         cursor: 'pointer'}}
+                onClick={onSearch}        
+
+                >
+              <SearchOutlined 
+                   style={{fontSize: '24px'}}
+                    
+                />
+            </div>              
+        </div>        
+    )
+}
